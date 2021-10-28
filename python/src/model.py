fb4d3b69fba17400d729468df8e197295b04be34
wjmgreen <wjmgreen16@gmail.com>
2021-10-28 15:42:39 -0500

    save

--- python/src/model.py
+++ python/src/model.py
@@ -16,16 +16,23 @@ class Report:
     # config
     HEADER = {'user-agent': 'University of Memphis'}
 
-    def __init__(self, company : Company, kpi : str):
+    def __init__(self, company : Company, kpi : str, **kwargs):
         self.company = company
         self.kpi = kpi
+        self.form = kwargs.get("form")
+        self.years = kwargs.get("years")
         self.data = self.get_data()
+
 
     def get_data(self):
         d = requests.get(f"https://data.sec.gov/api/xbrl/companyconcept/CIK{self.company.cik}/us-gaap/{self.kpi}.json",
                          headers=self.HEADER).json()
         df = pd.DataFrame.from_dict(d["units"]["USD"])
-        df = df.drop(["fy", "fp", "frame", "accn"], axis=1)
+        df = df.drop(["fy", "frame", "accn", "filed"], axis=1)
+        if self.form:
+            df = df[df["form"] == f"{self.form}"]
+        if self.years:
+            df = df[df["start"] == f"{self.years}"]
         return df
 
 
@@ -33,5 +40,10 @@ class Report:
 # Testing
 companies = [Company("Autozone Inc", "AZO", "0000866787"), Company("O REILLY AUTOMOTIVE INC", "ORLY", "0000898173")]
 
-myReport = Report(companies[1], "GrossProfit")
-print(myReport.data)
+myReport = Report(companies[1], "GrossProfit", form="10-K")
+df = myReport.data
+
+start, end = pd.to_datetime(df["start"]), pd.to_datetime(df["end"])
+df["start"], df["end"] = start, end
+df = df.drop_duplicates()
+print(df[df["start"] == "2015-01"] & df["end"] == "2015-12")
