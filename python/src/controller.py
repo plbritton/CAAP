ad9a720baf3f374954ec8c264d46ae5ed6a82ba0
canonmc <canonmc01@yahoo.com>
2021-12-02 20:56:20 -0600

    Organized widgets into separate files

--- python/src/controller.py
+++ python/src/controller.py
@@ -1,5 +1,7 @@
-from python.src.edp_ui import *
-import requests
+from python.gui.MainWindow import Window
+from PyQt5.QtWidgets import QApplication
+import sys
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     screen = Window()
