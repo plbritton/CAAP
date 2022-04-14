50af6949259fa2f1764bb2a9d33d53d618a7b26e
plbritton <45214415+plbritton@users.noreply.github.com>
2022-04-14 09:14:49 -0500

    Added ability to export to csv and excel

--- python/src/gui/MainWindow.py
+++ python/src/gui/MainWindow.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QTabWidget, QVBoxLayout, QMenuBar, QMainWindow, QAction, QMenu, QFileDialog
 from python.src.gui.Dashboard import Dashboard
 from python.src.gui.Processor import Processor
+import pandas as pd
 
 
 class MainWindow(QMainWindow):
@@ -20,16 +21,45 @@ class MainWindow(QMainWindow):
         # File menu
         fileMenu = QMenu("&File", self)
         menuBar.addMenu(fileMenu)
-        fileMenu.addAction(self.exportAction)
+        fileMenu.addAction(self.exportActionExcel)
+        fileMenu.addAction(self.exportActionCSV)
 
     def _createActions(self):
-        self.exportAction = QAction("&Export", self)
-        self.exportAction.triggered.connect(self.export)
+        self.exportActionExcel = QAction("&Export to Excel", self)
+        self.exportActionExcel.triggered.connect(self.exportExcel)
+        self.exportActionCSV = QAction("&Export to CSV", self)
+        self.exportActionCSV.triggered.connect(self.exportCSV)
 
-    def export(self):
+    def exportExcel(self):
         name = QFileDialog.getSaveFileName(self, 'Save File')
+        df = self.window.processor.data.copy(deep=True)
         try:
-            self.window.processor.data.to_excel(name)
+            df = df.astype(object)
+            df.to_excel(name[0])
+            print("Data export completed")
+            return
+        # This exception is when the user enters the file as anything but an xlsx file. 
+        except ValueError:
+            print("File must be saved as a .xlsx file.")
+            return
+            
+        except Exception:
+            print("No data to save")
+            return
+
+    def exportCSV(self):
+        name = QFileDialog.getSaveFileName(self, 'Save File')
+        df = self.window.processor.data.copy(deep=True)
+        try:
+            df = df.astype(object)
+            df.to_csv(name[0])
+            print("Data export completed")
+            return
+        # This exception is when the user enters the file as anything but a csv file. 
+        except ValueError:
+            print("File must be saved as a .csv file.")
+            return
+            
         except Exception:
             print("No data to save")
             return
